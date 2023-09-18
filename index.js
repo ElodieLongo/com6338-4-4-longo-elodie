9bccc6df189c6f432fb996d38b585674fab8735e
“Elodie <mail.elodielongo@gmail.com>
2023-09-18 08:37:10 +0200

    17 sept

---
+++ index.js
@@ -0,0 +1,31 @@
+var words = [
+  'bananas',
+  'grapes',
+  'carousel',
+  'milkshake',
+  'javascript',
+  'limousine',
+  'chocolate',
+  'programming',
+  'meatloaf',
+  'ukulele',
+  'mango'
+]
+
+
+var headline = document.querySelector("h2")
+console.log(headline)
+
+
+/* PSEUDOCODE
+- Select a random word from words array
+- pace it in the  #word-to-guess element with letters replaced by underscore
+- it should count 10 remaining guesses in the #remaining-guesses element. 
+-User pressed letter key (record that?)
+- Check if letter is one of the word to guess
+- IF included, replace the letter to the underscore
+- IF not included, add the letter to #incorrect-letters and subtract 1 from #remaining-guesses
+- When the last correct word is pressed, the game shoud count a win by adding 1 in the #wins element
+- After a win the game resets by choosing the next randomly-chosen word and reset all of the other elements
+- After a win the guessed word should appear in the #previous-word
+- After a loss (when remaining guesses are 0), the game resets and chooses the next random word and adds 1 to "losses"
