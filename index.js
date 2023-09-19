989394b5f26d565b9bffdd8b6ac30a0e40bdfb82
ElodieLongo <133866010+ElodieLongo@users.noreply.github.com>
2023-09-20 00:33:55 +0200

    20 sept

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var words = [
+let words = [
   'bananas',
   'grapes',
   'carousel',
@@ -12,19 +12,22 @@ var words = [
   'mango'
 ]
 
+window.onload = function(resetGame){
+
+}
 
 
-const randomWord = Math.floor(Math.random() * arr.length);
-console.log(randomWord)
 
-const word = arr[randomWord];
+function printRandomWord(){
+  
+  const randomWord = Math.floor(Math.random() * words.length);
+let printRandomWord =  document.querySelector("word-to-guess") = randomWord.replaceAll("[a-zA-Z]","_");
+};
 
-return word;
 
 
-console.log(randomWord)
 
-var headline = document.querySelector("h2")
+
 
 /* PSEUDOCODE
 - Select a random word from words array
@@ -38,4 +41,17 @@ var headline = document.querySelector("h2")
 - After a win the game resets by choosing the next randomly-chosen word and reset all of the other elements
 - After a win the guessed word should appear in the #previous-word
 - After a loss (when remaining guesses are 0), the game resets and chooses the next random word and adds 1 to "losses"
-  */
+
+
+MARKUP
+<div class="game">
+  <h2 id="word-to-guess"></h2>
+  <div>Previous word: <span id="previous-word"></span></div>
+  <div>Incorrect Letters: <span id="incorrect-letters"></span></div>
+  <div>Remaining Guesses: <span id="remaining-guesses"></span></div>
+  <div class="score">
+    <span>Wins: <span id="wins">0</span></span>
+    <span>Losses: <span id="losses">0</span></span>
+  </div>
+</div>
+*/
