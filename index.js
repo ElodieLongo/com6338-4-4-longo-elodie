591a77b8503761fcd94787be56544101da9508fe
“Elodie <mail.elodielongo@gmail.com>
2023-09-28 00:47:50 +0200

    27 sept

--- index.js
+++ index.js
@@ -22,121 +22,74 @@ document.addEventListener("DOMContentLoaded", function () {
   let lossesEl = document.getElementById("losses");
   let wins = 0;
   let losses = 0;
+  let randomWord = ""; // Initialize randomWord variable
 
   function printRandomWord() {
     const randomNumber = Math.floor(Math.random() * words.length);
-    const randomWord = words[randomNumber];
+    randomWord = words[randomNumber]; // Store the random word for checking
     const wordToGuess = "_".repeat(randomWord.length);
 
     let element = document.getElementById("word-to-guess");
     element.textContent = wordToGuess;
-   
-    remainingGuesses = 10; 
+
+    let remainingGuesses = 10;
     remainingGuessesEl.textContent = remainingGuesses;
   }
 
-  printRandomWord();
-});
-
-
-  
-//let key =  document.onKeyUp = function(e);
-//let key = e.key.toLowerCase()
-
-
-// access user keypress
-
-  
-// to reset at the end of game
-
-
-  
-/* PSEUDOCODE
-- Select a random word from words array
-- pace it in the  #word-to-guess element with letters replaced by underscore
-
-- it should count 10 remaining guesses in the #remaining-guesses element. 
--User pressed letter key (record that?)
-- Check if letter is one of the word to guess
-- IF included, replace the letter to the underscore
-- IF not included, add the letter to #incorrect-letters and subtract 1 from #remaining-guesses
-- When the last correct word is pressed, the game shoud count a win by adding 1 in the #wins element
-- After a win the game resets by choosing the next randomly-chosen word and reset all of the other elements
-- After a win the guessed word should appear in the #previous-word
-- After a loss (when remaining guesses are 0), the game resets and chooses the next random word and adds 1 to "losses"
-
-
-MARKUP
-<div class="game">
-  <h2 id="word-to-guess"></h2>
-  <div>Previous word: <span id="previous-word"></span></div>
-  <div>Incorrect Letters: <span id="incorrect-letters"></span></div>
-  <div>Remaining Guesses: <span id="remaining-guesses"></span></div>
-  <div class="score">
-    <span>Wins: <span id="wins">0</span></span>
-    <span>Losses: <span id="losses">0</span></span>
-  </div>
-</div>
-*/
-
-/* Notes from course
-To change DOM elements:
-
-Text example:
-let element =  document.querySelector("#word-to-guess");
-element.textContent = "text I want to see"
-
-Image example:
-let image = document.querySelector("image")
-img.src = "http://picsum.photo/600/300"
-
-Inner HTML To add a string of HTML here replace a p with a list:
-let secondP = document.querySelector("p:nth-of-type(2)")
-secondP.innerHTML = "<ol>"
-<li>red</li>
-<li>blue</li>
-<li>yellow</li>
-</ol>
-
-To create and append elements:
-let or = document.createElement("ol")
-document.body.appendChild(ol)
-
-To remove elements from DOM
-var lisItems = document.querySelectorAll("ol > li")
-for(let listitem of lisitems) {
-  if (listItem.textContent === "broccoli") {
-    listItem.remove()
+  function updateWordToGuess(correctGuesses) {
+    const wordArray = randomWord.split("");
+    for (let i = 0; i < randomWord.length; i++) {
+      if (!correctGuesses.includes(wordArray[i])) {
+        wordArray[i] = "_";
+      }
+    }
+    return wordArray.join("");
   }
 
-  Click Events:
-The old way is with onClick
-The new way:
-  btn.addEventListener("click".handleClick) 
-  
-  function handleClick(){
+  let correctGuesses = [];
+
+  function handleKeyPress(key) {
+    key = key.toLowerCase();
+    if (randomWord.indexOf(key) === -1) {
+      // Incorrect guess
+      if (!incorrectLettersEl.textContent.includes(key)) {
+        incorrectLettersEl.textContent += key + " ";
+        remainingGuessesEl.textContent--;
+      }
+      if (parseInt(remainingGuessesEl.textContent) === 0) {
+        // If remaining guesses reach 0, it's a loss
+        losses++;
+        lossesEl.textContent = losses;
+        previousWordEl.textContent = randomWord;
+        resetGame();
+      }
+    } else {
+      // Correct guess
+      if (!correctGuesses.includes(key)) {
+        correctGuesses.push(key);
+        const updatedWord = updateWordToGuess(correctGuesses);
+        document.getElementById("word-to-guess").textContent = updatedWord;
+        if (!updatedWord.includes("_")) {
+          // If there are no underscores left, it's a win
+          wins++;
+          winsEl.textContent = wins;
+          previousWordEl.textContent = randomWord;
+          resetGame();
+        }
+      }
     }
-  
- btn.removeEventListener("click".handleClick) 
+  }
 
-To attach the same behaviour to multipl elements (here to all buttons)
-let btns = document.querySelectorAll(".buttons > button")
-let span = document.getElementById("clicked")
-for (let i = 0; i < btns.lenght; i++) {
-  btns[i].addEventListener("click".function() {
-    span.textContent = this.textContent
-  })
-}
+  function resetGame() {
+    correctGuesses = [];
+    incorrectLettersEl.textContent = "";
+    remainingGuessesEl.textContent = "10";
+    printRandomWord();
+  }
 
-How to record keyboard events:
-grab h2 tag:
-var h2 = document.querySelector("h2")
-var pre = document.querySelector("pre")
+  printRandomWord();
 
-document.onkeyup =function(e) {
-h2.textContent = e.key
-pre.textContent = JSON.stringify(e. null.2){
-  key:e.
-})
-}
-*/
+  document.onkeyup = function (e) {
+    handleKeyPress(e.key);
+  };
+});
