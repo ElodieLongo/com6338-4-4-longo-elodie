2c3d04ca09b4d93a789d2c68d9f114ece2df1daf
“Elodie <mail.elodielongo@gmail.com>
2023-09-28 23:38:33 +0200

    Assignment 4

--- index.js
+++ index.js
@@ -1,95 +1,90 @@
+var words = [ 'bananas', 'grapes', 'carousel', 'milkshake', 'javascript', 'limousine', 'chocolate', 
+'programming', 'meatloaf', 'ukulele', 'mango' ];
+
+const score = {
+  wins: 0,
+  losses: 0,
+  previousWord: null
+}
+
+const singleGame = {
+  word: null,
+  remainingGuesses: 10,
+  incorrectLettersArr: [],
+  guessedLettersArr: []
+}
+
+let previousWordEl, incorrectLettersEl, remainingGuessesEl, winsEl, lossesEl, wordToGuessEl;
+
 document.addEventListener("DOMContentLoaded", function () {
+  previousWordEl = document.getElementById("previous-word");
+  incorrectLettersEl = document.getElementById("incorrect-letters");
+  remainingGuessesEl = document.getElementById("remaining-guesses");
+  winsEl = document.getElementById("wins");
+  lossesEl = document.getElementById("losses");
+  wordToGuessEl = document.getElementById("word-to-guess");
 
-  let words = [
-    'bananas',
-    'grapes',
-    'carousel',
-    'milkshake',
-    'javascript',
-    'limousine',
-    'chocolate',
-    'programming',
-    'meatloaf',
-    'ukulele',
-    'mango'
-  ];
-
-  // Get references to HTML elements
-  let previousWordEl = document.getElementById("previous-word");
-  let incorrectLettersEl = document.getElementById("incorrect-letters");
-  let remainingGuessesEl = document.getElementById("remaining-guesses");
-  let winsEl = document.getElementById("wins");
-  let lossesEl = document.getElementById("losses");
-  let wins = 0;
-  let losses = 0;
-  let randomWord = ""; // Initialize randomWord variable
-
-  function printRandomWord() {
-    const randomNumber = Math.floor(Math.random() * words.length);
-    randomWord = words[randomNumber]; // Store the random word for checking
-    const wordToGuess = "_".repeat(randomWord.length);
-
-    let element = document.getElementById("word-to-guess");
-    element.textContent = wordToGuess;
-
-    let remainingGuesses = 10;
-    remainingGuessesEl.textContent = remainingGuesses;
-  }
-
-  function updateWordToGuess(correctGuesses) {
-    const wordArray = randomWord.split("");
-    for (let i = 0; i < randomWord.length; i++) {
-      if (!correctGuesses.includes(wordArray[i])) {
-        wordArray[i] = "_";
-      }
-    }
-    return wordArray.join("");
-  }
-
-  let correctGuesses = [];
-
-  function handleKeyPress(key) {
-    key = key.toLowerCase();
-    if (randomWord.indexOf(key) === -1) {
-      // Incorrect guess
-      if (!incorrectLettersEl.textContent.includes(key)) {
-        incorrectLettersEl.textContent += key + " ";
-        remainingGuessesEl.textContent--;
-      }
-      if (parseInt(remainingGuessesEl.textContent) === 0) {
-        // If remaining guesses reach 0, it's a loss
-        losses++;
-        lossesEl.textContent = losses;
-        previousWordEl.textContent = randomWord;
-        resetGame();
-      }
-    } else {
-      // Correct guess
-      if (!correctGuesses.includes(key)) {
-        correctGuesses.push(key);
-        const updatedWord = updateWordToGuess(correctGuesses);
-        document.getElementById("word-to-guess").textContent = updatedWord;
-        if (!updatedWord.includes("_")) {
-          // If there are no underscores left, it's a win
-          wins++;
-          winsEl.textContent = wins;
-          previousWordEl.textContent = randomWord;
-          resetGame();
-        }
-      }
-    }
-  }
-
-  function resetGame() {
-    correctGuesses = [];
-    incorrectLettersEl.textContent = "";
-    remainingGuessesEl.textContent = "10";
-    printRandomWord();
-  }
-
-  printRandomWord();
-
+  startSingleGame();
+  
   document.onkeyup = function (e) {
     handleKeyPress(e.key);
   };
+  
 });
+
+function startSingleGame(previousWord = null) {
+  previousWordEl.textContent = previousWord;
+  singleGame.word = words[Math.floor(Math.random() * words.length)];
+  console.log(singleGame.word)
+  wordToGuessEl.textContent = "_".repeat(singleGame.word.length);
+  remainingGuessesEl.textContent = 10;
+  incorrectLettersEl.textContent = "";
+  singleGame.remainingGuesses = 10;
+  singleGame.incorrectLettersArr = [];
+  singleGame.guessedLettersArr = [];
+}
+
+
+
+function handleKeyPress(key) {
+  key = key.toLowerCase();
+  const isALetter = key.match(/^[a-z]$/);
+
+  if (isALetter && !singleGame.incorrectLettersArr.includes(key) && 
+   !singleGame.guessedLettersArr.includes(key)) {
+
+    if (singleGame.word.includes(key)) {
+      singleGame.guessedLettersArr.push(key);
+      const strAsArr = singleGame.word.split("");
+      const wordToGuessAsArr = wordToGuessEl.textContent.split("");
+
+      strAsArr.forEach((letter, i) => {
+        if (key === letter) {
+          wordToGuessAsArr[i] = key;
+        }
+      });
+
+      wordToGuessEl.textContent = wordToGuessAsArr.join('');
+
+      if (strAsArr.every(letter => singleGame.guessedLettersArr.includes(letter))) {
+        score.wins++;
+        winsEl.textContent = score.wins;
+        startSingleGame(singleGame.word);
+      }
+
+    } else {
+      singleGame.incorrectLettersArr.push(key);
+      incorrectLettersEl.textContent += " " + key;
+      singleGame.remainingGuesses--;
+      remainingGuessesEl.textContent--;
+
+      if (singleGame.remainingGuesses === 0) {
+        score.losses++;
+        lossesEl.textContent = score.losses;
+        startSingleGame(singleGame.word);
+      }
+    }
+
+  }
+
+}
